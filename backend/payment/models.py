00c07d98a491d0ad0b40ad68913613a5368a6a53
optimus-glitch <ccarlito7641@gmail.com>
2022-03-11 11:45:25 +0300

    Added payment model, registered Payment model, created view for payment/

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+PAYMENT_MODES= [
+    ('mpesa', 'Mpesa'),
+    ('paypal', 'Paypal'),
+    ]
+
+
+class Payment(models.Model):
+    student_id = models.CharField(max_length=30, default='')
+    first_name= models.CharField(max_length=100, default='')
+    last_name= models.CharField(max_length=100, default='')
+    room_id = models.CharField(max_length=20, default='')
+    room_charge = models.FloatField(null=False, default=0.0)
