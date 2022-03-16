ba2acd4860bc32df784d1b68fed1542c811978a0
wandabi-gs <wandabigideon@gmail.com>
2022-03-16 19:34:41 +0300

    Added Room urls,views and model

--- backend/room/views.py
+++ backend/room/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Room
+from hostel.models import Hostel
+from django.http import JsonResponse
 
-# Create your views here.
+def add_room(request):
+    if request.method == "POST":
+        proceed = True
+        checkhostel = Hostel.objects.filter(hostel_name=request.POST['hostel_name'])
+        if checkhostel:
+            proceed = False
+            return JsonResponse({'HostelError':request.POST['hostel_name']+' is not a registered hostel'})
+        checkroom = Room.objects.filter(hostel__hostel_name=request.POST['hostel_name'],room_number=request.POST['room_number'])
+        if checkroom:
+            proceed = False
+            return JsonResponse({'RoomError':'Room already exists'})
+        if proceed:
+            room = Room()
+            room.hostel = Hostel.objects.get(hostel_name=request.POST['hostel_name'])
+            room.room_number = request.POST['room_number']
+            room.room_price = request.POST['room_price']
+            room.max_occupants = request.POST['max_occupants']
+            room.save()
+            return JsonResponse({'success':'Room added succesfully'})
+
+
+def remove_room(request):
+    if request.method == "POST":
+        proceed = True
+        checkhostel = Hostel.objects.filter(hostel_name=request.POST['hostel_name'])
+        if checkhostel:
+            proceed = False
+            return JsonResponse({'HostelError':request.POST['hostel_name']+' is not a registered hostel'})
+        checkroom = Room.objects.filter(hostel__hostel_name=request.POST['hostel_name'],room_number=request.POST['room_number'])
+        if not checkroom:
+            proceed = False
+            return JsonResponse({'RoomError':'Room does not exist'})
+        if proceed:
+            room = Room.objects.get(hostel__hostel_name=request.POST['hostel_name'],room_number=request.POST['room_number'])
+            room.delete()
+            return JsonResponse({'success':'Room removed succesfully'})
