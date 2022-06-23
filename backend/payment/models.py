2a3c3f08d985972d96f60febc15c4a6567d31667
day qinyanjui <davidkinyanjui052@gmail.com>
2022-06-23 11:12:39 +0300

    Update models.py

--- backend/payment/models.py
+++ backend/payment/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from hostel.models import TrackingModel
 
 # Create your models here.
 PAYMENT_MODES= [
@@ -8,9 +8,12 @@ PAYMENT_MODES= [
     ]
 
 
-class Payment(models.Model):
+class Payment(TrackingModel):
     student_id = models.CharField(max_length=30, default='')
     first_name= models.CharField(max_length=100, default='')
     last_name= models.CharField(max_length=100, default='')
     room_id = models.CharField(max_length=20, default='')
     room_charge = models.FloatField(null=False, default=0.0)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
