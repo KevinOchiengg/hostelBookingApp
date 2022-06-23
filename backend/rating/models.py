1e5d9c5c266224d6897708ead651b90e51839c22
day qinyanjui <davidkinyanjui052@gmail.com>
2022-06-23 11:15:42 +0300

    Update models.py

--- backend/rating/models.py
+++ backend/rating/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from user.models import CustomUser
 from django.core.validators import MaxValueValidator, MinValueValidator
-from hostel.models import Hostel
-# from djangoratings.fields import RatingField
-# Create your models here.
+from hostel.models import Hostel,TrackingModel
 
-class Rating(models.Model):
+class Rating(TrackingModel):
  
-    user = models.ForeignKey(CustomUser , models.CASCADE )
-    hostel = models.ForeignKey(Hostel , models.CASCADE )
+    user = models.ForeignKey(CustomUser , on_delete=models.CASCADE )
+    hostel = models.ForeignKey(Hostel , on_delete=models.CASCADE )
     subject = models.CharField(max_length=10, default='optional')
     comment = models.TextField(max_length=250)
     rate = models.IntegerField(default=0,
@@ -17,10 +15,6 @@ class Rating(models.Model):
             MinValueValidator(0),
         ]
     )
-
-    # status = models.CharField(max_length=10,choices=STATUS , default='New')
-    # models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True) 
+ 
     def __str__(self) -> str:
         return self.subject
