49dcd296888d879ad7df442c9932bedc0a6c0eb9
day qinyanjui <davidkinyanjui052@gmail.com>
2022-06-23 11:07:10 +0300

    Update models.py

--- backend/hostel/models.py
+++ backend/hostel/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Hostel(models.Model):
+
+class TrackingModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at']
+        
+class Hostel(TrackingModel):
 
     hostel_name = models.CharField(max_length=50 )
     Number_of_rooms = models.CharField(max_length=10)
     description = models.CharField(max_length=250 , default='')
     Address = models.CharField(max_length=50 ,null=True)    
     image = models.ImageField(upload_to = 'images/', blank=True, null=True, default='') 
-    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.hostel_name
